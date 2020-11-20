5245018f527e2f4d190f0736b43754e667b9b0c2
YuikoIto <toffywing@gmail.com>
2020-11-20 17:17:39 +0900

    change: リネイム

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <v-app>
     <Header/>
     <v-main>
-      <HelloWorld/>
+      <Main/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Main from './components/Main';
 import Header from './components/Header';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Main,
     Header
   },
 
