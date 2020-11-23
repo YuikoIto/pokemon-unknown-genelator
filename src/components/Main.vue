990eede382ebe59c10ff5b63fd0feb0158e3644a
YuikoIto <toffywing@gmail.com>
2020-11-23 16:01:36 +0900

    add: 説明文を追加

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,27 @@
 <template>
   <v-container>
+    <v-card
+     class="mx-auto"
+     >
+     <v-card-text>
+       <h2>アンノーン文字変換</h2>
+       <div>これは簡単にアンノーン文字に変換できるサービスです。アンノーン文字で書いたものを共有しよう</div>
+     </v-card-text>
+    </v-card>
     <v-row>
       <v-col cols="12" sm="12" md="6">
         <v-text-field
-          @change="changeText"
+          v-model="text"
           label="原文"
-          placeholder="アンノーン文字にしたいものを入力してください"></v-text-field>
-
+          placeholder="アンノーン文字にしたいものを入力してください"
+        ></v-text-field>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12" sm="12" md="6">
         <p>アンノーン文字</p>
         <div class = "unknownStyle">
-          {{ this.text }}
+          {{ text }}
         </div>
       </v-col>
     </v-row>
@@ -33,9 +41,6 @@
       text: ''
     }),
     methods: {
-      changeText(e) {
-        this.text = e;
-      }
     }
   }
 </script>
